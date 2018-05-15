7c1de35de6fddd054d60b4e17b752eee458f3236
Mikaiyl <mikaiyl.davis@icloud.com>
2018-05-15 15:57:09 -0400

    Done with flex

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,10 @@ const state = {
     charCol: 0,
     charRow: 9,
     maze: document.getElementById("maze"),
+    charachter: document.createElement("div"),
 }
+
+state.charachter.id = "running-man";
 
     /*
      *      Setup
@@ -33,26 +36,31 @@ const state = {
 */
 
 const printMazeToDOM = () => {
-    for (let mazeArrayRowIndex in mazeArray) {
-        for (let mazeArrayColumnIndex in mazeArray[mazeArrayRowIndex] ){
-            // create elements add 1 so css grid starts at 1 not 0
-            let row = parseInt(mazeArrayRowIndex) + 1;
-            let column = parseInt(mazeArrayColumnIndex) + 1;
-            let cell = document.createElement("div");
-            cell.style.gridRow = row;
-            cell.style.gridColumn = column;
+    for ( let mazeArrayRowIndex in mazeArray ) {
+
+        let DOMRow = document.createElement("div");
+        DOMRow.id = "r" + mazeArrayRowIndex;
+        DOMRow.classList.add("row");
+        
+        for ( let mazeArrayColumnIndex in mazeArray[mazeArrayRowIndex] ){
+            // create elements for flexbox
+            let rowIndex = parseInt(mazeArrayRowIndex);
+            let columnIndex = parseInt(mazeArrayColumnIndex);
+
+            let DOMcell = document.createElement("div");
+            DOMcell.classList.add("cell");
+            
+            DOMcell.classList.add("r" + mazeArrayRowIndex);
+            DOMcell.classList.add("c" + mazeArrayColumnIndex);
 
             if ( mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "W" ) {
-                cell.style.zIndex = 2;
-                cell.dataset.wall = "true";
-            } else if ( mazeArray[mazeArrayRowIndex][mazeArrayColumnIndex] === "C" ) {
-                cell.id = "running-man";
+                DOMcell.dataset.wall = true;
             }
-
-            state.maze.appendChild(cell);
+            DOMRow.appendChild(DOMcell);
         }
+        state.maze.appendChild(DOMRow);
     }
-    state.charachter = document.getElementById("running-man");
+    state.maze.querySelector("div#r9 div.c0").appendChild(state.charachter);
 }
 
 /*
@@ -63,28 +71,28 @@ document.addEventListener('keydown', (event) => {
     const keyName = event.key;
 
     if ( keyName == "ArrowDown" ) {
-        if ( mazeArray[state.charRow+1][state.charCol] !== "W") {
+        if ( mazeArray[state.charRow+1][state.charCol] !== "W" && state.charRow < 14) {
             state.charRow += 1;
-            state.charachter.style.gridRow = state.charRow + 1;
+            state.maze.querySelector("div.cell.r" + state.charRow + ".c" + state.charCol).appendChild(state.charachter);
             state.charachter.style.transform = "rotate(0.25turn)";
         }
     } else if ( keyName == "ArrowUp" ) {
-        if ( mazeArray[state.charRow-1][state.charCol] !== "W" ) {        
+        if ( mazeArray[state.charRow-1][state.charCol] !== "W" && state.charRow > 0) {        
             state.charRow -= 1;
-            state.charachter.style.gridRow = state.charRow + 1;
+            state.maze.querySelector("div.cell.r" + state.charRow + ".c" + state.charCol).appendChild(state.charachter);
             state.charachter.style.transform = "rotate(-0.25turn)";
         }
     } else if ( keyName == "ArrowLeft" ) {
-        if ( mazeArray[state.charRow][state.charCol-1] !== "W" ) {        
+        if ( mazeArray[state.charRow][state.charCol-1] !== "W" && state.charCol > 0) {        
             state.charCol -= 1;
-            state.charachter.style.gridColumn = state.charCol + 1;
+            state.maze.querySelector("div.cell.r" + state.charRow + ".c" + state.charCol).appendChild(state.charachter);
             state.charachter.style.transform = "scaleX(-1)";
         }
     } else if ( keyName == "ArrowRight" ) {
-        if ( mazeArray[state.charRow][state.charCol+1] !== "W" ) {        
+        if ( mazeArray[state.charRow][state.charCol+1] !== "W" && state.charCol < 20) {        
             state.charCol += 1;
-            state.charachter.style.gridColumn = state.charCol + 1;
             state.charachter.style.transform = "rotate(0turn)";
+            state.maze.querySelector("div.cell.r" + state.charRow + ".c" + state.charCol).appendChild(state.charachter);
         }
     }
 });
@@ -100,6 +108,21 @@ const win = () => {
     alert("You won!");
     state.maze.innerHTML = "";
     printMazeToDOM();
+    state.charCol = 0;
+    state.charRow = 9; 
+}
+
+// const animate = ( dir, next) => {
+//     if (dir === "N") {
+//         state.charachter.classList.remove("animateE");
+//     } else if (dir === "S") {
+
+//         state.charachter.classList.remove("animateE");
+//     } else if (dir === "S") {
+//         state.charachter.classList.remove("animateE");
+//     } else if (dir === "S") {
+//         state.charachter.classList.remove("animateE");
+//     } else if (dir === "S") {
 }
 
     /*
